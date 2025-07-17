a88b9142879997da0fec8fa8770c136dacc4fda7
Sofia Cobo <sofiamariacobo@gmail.com>
2025-07-17 10:27:11 -0300

    se agregan routers con endpoints de usuario

--- auth/jwt.py
+++ auth/jwt.py
@@ -1,1 +1,9 @@
-#codigo
+from database import SessionLocal
+
+
+def get_db(): #crea la conexion
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from database import Base, engine
+from routers import auth
 
 #Para crear las tablas de la bd 
 Base.metadata.create_all(bind=engine)
 
 # Creamos la app
 app = FastAPI()
+
+#Incluimos los routers
+app.include_router(auth.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "La app funciona correctamente"}
